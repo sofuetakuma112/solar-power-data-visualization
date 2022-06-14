86b744713cb3f1582be6189e7cb5347b3dc8f227
sofuetakuma <kawahagi0620@gmail.com>
2022-06-14 16:42:33 +0900

    相互相関の計算の際に、任意の幅で計算値のデータ列をスライドさせれるよう変更

--- correlogram.py
+++ correlogram.py
@@ -1,4 +1,5 @@
 import datetime
+from operator import itemgetter
 from es import fetch
 from utils.file import getPickleFilePathByDatetime
 from utils.numerical_processing import min_max
@@ -12,6 +13,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 import japanize_matplotlib
 import copy
+import math
+
+
+def loadQAndDtForPeriod(startDt, span):
+    Q_all = []
+    dt_all = []
+    dt_crr = startDt
+
+    span_float, span_int = math.modf(span)
+    loopCount = 0
+    while True:
+        filePath = getPickleFilePathByDatetime(dt_crr)
+        with open(filePath, "rb") as f:
+            docs = pickle.load(f)
+        docs = sortDocsByKey(docs, "JPtime")
+        jptimes = extractFieldsFromDocs(docs, "JPtime")
+        dts_per_day = isoformats2dt(jptimes)
+        if loopCount == 0:
+            lastDt = (
+                dts_per_day[0]  # dts_per_day[0]から{fixedDaysLen}日後をlastDtとする
+                + datetime.timedelta(days=span_int)
+                + datetime.timedelta(hours=span_float * 24)
+            )
+            print(f"lastDt: {lastDt}")
+        Qs_per_day = extractFieldsFromDocs(docs, "solarIrradiance(kw/m^2)")
+
+        if all(list(map(lambda dt: dt > lastDt, dts_per_day))):
+            # jptimesの全ての日時がlastDtより未来の日時
+            indexes = [i for i, _ in enumerate(Q_all) if dt_all[i] <= lastDt]
+            Q_all = itemgetter(*indexes)(Q_all)
+            dt_all = itemgetter(*indexes)(dt_all)
+            break
+
+        Q_all = list(chain(Q_all, Qs_per_day))
+        dt_all = list(chain(dt_all, dts_per_day))
+
+        if len(Qs_per_day) < 10:
+            print(f"データがない日: {dt_crr}")
+
+        dt_crr = dt_crr + datetime.timedelta(days=1)
+        loopCount += 1
+
+    return [dt_all, Q_all]
 
 
 def loadQAndDtForAGivenPeriod(fromDt, toDt):
@@ -40,6 +84,9 @@ def loadQAndDtForAGivenPeriod(fromDt, toDt):
 
 
 def getDtListAndCoefBeCompleted(dts, qs):
+    """
+    補完した日時データと日射量の増分に対する係数のリストを取得する
+    """
     comps = []
     for i in range(len(dts)):
         if len(dts) - 1 == i:  # 最後の要素を参照するインデックスの場合、次の要素の参照時にエラーが起こるのでスキップ
@@ -63,6 +110,9 @@ def getDtListAndCoefBeCompleted(dts, qs):
 
 
 def unifyDeltasBetweenDts(dts, qs):
+    """
+    補完した日時データとそれに対応した日射量のリストを取得する
+    """
     # 補完データを取得する
     comps = getDtListAndCoefBeCompleted(dts, qs)
     # 補完データと既存のデータをマージする
@@ -86,6 +136,9 @@ def unifyDeltasBetweenDts(dts, qs):
 
 
 def testEqualityDeltaBetweenDts(dts, delta=1.0):
+    """
+    日時データのdeltaが1[s]で統一されているかテストする
+    """
     for i in range(len(dts)):
         if len(dts) - 1 == i:
             break
@@ -103,6 +156,8 @@ def main():
 
     fromDtStr = args[1].split("/")
     toDtStr = args[2].split("/")
+    fixedDaysLen = float(args[3])
+    dynamicDaysLen = float(args[4])
 
     fromDt = datetime.datetime(int(fromDtStr[0]), int(fromDtStr[1]), int(fromDtStr[2]))
     toDt = datetime.datetime(int(toDtStr[0]), int(toDtStr[1]), int(toDtStr[2]))
@@ -110,9 +165,13 @@ def main():
     fetch.fetchDocsByPeriod(fromDt, toDt)
 
     # 与えた期間の日射量と計測日時をファイルから読み込む(dtでソート済み)
-    dt_all, Q_all = loadQAndDtForAGivenPeriod(fromDt, toDt)
-    dt_all_copy = copy.deepcopy(dt_all)
-    Q_all_copy = copy.deepcopy(Q_all)
+    # dt_all, Q_all = loadQAndDtForAGivenPeriod(fromDt, toDt) # TODO: スタートの日付と、何日分取得するかの期間を引数に取得する関数を実装する
+    dt_all, Q_all = loadQAndDtForPeriod(fromDt, fixedDaysLen)
+    dt_all_copy = copy.deepcopy(dt_all) # 補完が正しく行えているか確認する用
+    Q_all_copy = copy.deepcopy(Q_all) # 補完が正しく行えているか確認する用
+
+    print(f"dt_all[0]: {dt_all[0]}")
+    print(f"dt_all[-1]: {dt_all[-1]}")
 
     # 時系列データのデルタを均一にする
     dt_all, Q_all = unifyDeltasBetweenDts(dt_all, Q_all)
@@ -120,49 +179,40 @@ def main():
     # 時系列データの点間が全て1.0[s]かテストする
     testEqualityDeltaBetweenDts(dt_all)
 
-    endDt = (
-        dt_all[0] + datetime.timedelta(days=7) + datetime.timedelta(hours=12)
-    )  # スタートから7.5日後を終わりの日時にする
-    print(f"dt_all列の最大値: {endDt}")
-    last_dt_index = 0
-    for i, dt in enumerate(dt_all):
-        if dt > endDt:
-            last_dt_index = i
-            break
-
-    # 7.5日の範囲になるよう切り取る
-    dt_all = dt_all[:last_dt_index]
-    Q_all = Q_all[:last_dt_index]
-
-    q_calc_end_dt = dt_all[0] + datetime.timedelta(days=7)  # スタートの日時から7日後
+    q_calc_end_dt = dt_all[0] + datetime.timedelta(
+        days=dynamicDaysLen
+    )  # スタートの日時から{dynamicDaysLen}日後
     q_calc_end_dt_index = 0
     for i, dt_crr in enumerate(dt_all):
         if dt_crr > q_calc_end_dt:
             q_calc_end_dt_index = i
             break
-    print(f"dt_all列の先頭の日時から7日後の日時: {dt_all[q_calc_end_dt_index]}")
+    print(f"dt_all列の先頭の日時から{dynamicDaysLen}日後の日時: {dt_all[q_calc_end_dt_index]}")
+
+    dtStartLag_float, dtStartLag_int = math.modf((fixedDaysLen - dynamicDaysLen) / 2)
 
     # Q_calc_allの時系列データを実測値の時系列データより6時間進める
-    # 相互コレログラムを計算する際、計算値を6時間(0.25日)シフトさせたタイミングで計算値と実測値の時系列データのズレが消える
+    # 相互コレログラムを計算する際、計算値を{(fixedDaysLen - dynamicDaysLen) * 24 / 2}時間({(fixedDaysLen - dynamicDaysLen) / 2}日)シフトさせたタイミングで計算値と実測値の時系列データのズレが消える
     dts_q_calc_all = dt_all[:q_calc_end_dt_index]
-    dts_q_calc_all_with_6hours_delay = list(
+    dts_q_calc_all_with_lag = list(
         map(
-            lambda dt: dt + datetime.timedelta(hours=6),
+            lambda dt: dt
+            + datetime.timedelta(days=dtStartLag_int)
+            + datetime.timedelta(hours=dtStartLag_float * 24),
             dts_q_calc_all,
         )
     )
 
-    print(f"dt_all[0]: {dt_all[0]}")
-    print(f"dt_all[-1]: {dt_all[-1]}")
-    print(f"dts_q_calc_all_with_6hours_delay[0]: {dts_q_calc_all_with_6hours_delay[0]}")
     print(
-        f"dts_q_calc_all_with_6hours_delay[-1]: {dts_q_calc_all_with_6hours_delay[-1]}"
-    )  # 0.25日(6時間ずらした状態からスタートして、トータル12時間シフトさせる)
-    Q_calc_all = list(  # Q_allの時刻データ列より6時間遅れた時刻データ列から計算
+        f"dts_q_calc_all_with_{(fixedDaysLen - dynamicDaysLen) * 24 / 2}hours_delay[0]: {dts_q_calc_all_with_lag[0]}"
+    )
+    print(
+        f"dts_q_calc_all_with_{(fixedDaysLen - dynamicDaysLen) * 24 / 2}hours_delay[-1]: {dts_q_calc_all_with_lag[-1]}"
+    )
+    Q_calc_all = list(
         map(
-            # lambda dt: calcQ(dt, 33.82794, 132.75093) / 1000,
             lambda dt: max(calcQ(dt, 33.82794, 132.75093), 0) / 1000,
-            dts_q_calc_all_with_6hours_delay,
+            dts_q_calc_all_with_lag,
         )
     )
 
@@ -170,18 +220,17 @@ def main():
 
     print(f"corr.argmax(): {corr.argmax()}")
     print(
-        f"dts_q_calc_all_with_6hours_delay[corr.argmax()]: {dts_q_calc_all_with_6hours_delay[corr.argmax()]}"
+        f"dts_q_calc_all_with_lag[corr.argmax()]: {dts_q_calc_all_with_lag[corr.argmax()]}"
     )
-    tmp = (
-        dts_q_calc_all_with_6hours_delay[corr.argmax()]
-        - dts_q_calc_all_with_6hours_delay[0]
-    )
+    tmp = dts_q_calc_all_with_lag[corr.argmax()] - dts_q_calc_all_with_lag[0]
     print(f"best delta: {np.abs(6 * 60 * 60 - tmp.total_seconds())}")
 
-    axes = [plt.subplots()[1] for i in range(2)]
+    # axes = [plt.subplots()[1] for i in range(2)]
+    axes = [plt.subplots()[1] for i in range(1)]
 
-    axes[0].plot(dt_all, Q_all, label="実測値(補完)")  # 実データをプロット
-    axes[0].plot(dt_all_copy, Q_all_copy, label="実測値(生データ)", linestyle="dashed")
+    # axes[0].plot(dt_all, Q_all, label="実測値(補完)")  # 実データをプロット
+    # axes[0].plot(dt_all_copy, Q_all_copy, label="実測値(生データ)", linestyle="dashed")
+
     # axes[0].set_xlim(
     #     [
     #         datetime.datetime(dt_all[0].year, dt_all[0].month, dt_all[0].day, 0, 0, 0),
@@ -190,18 +239,25 @@ def main():
     #     ]
     # )
     # axes[0].plot(
-    #     dts_q_calc_all_with_6hours_delay,
+    #     dts_q_calc_all_with_lag,
     #     Q_calc_all,
     #     label="計算値",
     # )  # 実データをプロット
-    axes[0].set_xlabel("日時")
-    axes[0].set_ylabel("日射量[kW/m^2]")
+    # axes[0].set_xlabel("日時")
+    # axes[0].set_ylabel("日射量[kW/m^2]")
 
     print(f"len(corr): {len(corr)}")
 
-    axes[1].set_xlabel("ラグ")
-    axes[1].set_ylabel("相互相関")
-    axes[1].plot(np.arange(len(corr)), corr, color="r")
+    axes[0].set_xlabel("実測値の日時 - 計算値の日時[s]")
+    axes[0].set_ylabel("相互相関")
+    axes[0].plot(
+        [
+            i - (fixedDaysLen - dynamicDaysLen) * 24 * 60 * 60 / 2
+            for i in range(len(corr))
+        ],
+        corr,
+        color="r",
+    )
 
     plt.show()
 
