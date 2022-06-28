b3930f72c4c0331dbd6b14a4976446e4fe71a79b
sofuetakuma <kawahagi0620@gmail.com>
2022-06-28 15:05:23 +0900

    一度相互相関が最大となるラグだけ計算値の用の日時をスライドさせた日時列をもとに任意の係数を掛け合わせて日射量を求めた後、再度相互相関が最大となるラグを各係数ごとに求める処理を実装

--- search_min_lag.py
+++ search_min_lag.py
@@ -8,18 +8,51 @@ import math
 from correlogram import loadQAndDtForPeriod, unifyDeltasBetweenDts
 
 
-def calcLag(Q_all, dts_q_calc_all_applied_lag, coef=1):
-    Q_calc_all_applied_lag = list(
+def calcLag(Q_all, dts_for_calc, coef=1):
+    """
+    実測値と計算値の日射量データをもとに相互相関が最大となるラグ[s]を返す
+    Parameters
+    ----------
+    Q_all : array-like
+        実測値の日射量
+    dts_for_calc : int
+        計算値を算出する用の日時データ(スライド全量の半分だけ日時を進めている必要がある)
+    coef : float
+        日射量計算時に掛ける係数
+    """
+    Q_for_calc = list(
         map(
             lambda dt: max(calcQ(dt, 33.82794, 132.75093), 0) * coef / 1000,
-            dts_q_calc_all_applied_lag,
+            dts_for_calc,
         )
     )
 
-    corr = np.correlate(Q_all, Q_calc_all_applied_lag)
+    corr = np.correlate(Q_all, Q_for_calc)
 
     largest_lag_sec = 6 * 60 * 60 - corr.argmax()  # 真の計算値の時間 - 実測値の時間
     return largest_lag_sec
+
+
+def shiftDts(dts, days, hour_coef):
+    """
+    指定した期間だけ日時データを進める(遅らせる)
+    Parameters
+    ----------
+    dts : array-like
+        日付データ
+    days : int
+        進める(遅らせる)日数
+    hour_coef : float
+        24時間のうち、何時間進める(遅らせる)かの係数
+    """
+    return list(
+        map(
+            lambda dt: dt
+            + datetime.timedelta(days=days)
+            + datetime.timedelta(hours=hour_coef * 24),
+            dts,
+        )
+    )
 
 
 def main():
@@ -53,35 +86,32 @@ def main():
 
     # Q_calc_allの時系列データを実測値の時系列データより6時間進める
     # 相互コレログラムを計算する際、計算値を{(fixedDaysLen - dynamicDaysLen) * 24 / 2}時間({(fixedDaysLen - dynamicDaysLen) / 2}日)シフトさせたタイミングで計算値と実測値の時系列データのズレが消える
-    dts_q_calc_all = dt_all[:q_calc_end_dt_index]
-    dts_q_calc_all_applied_lag = list(
-        map(
-            lambda dt: dt
-            + datetime.timedelta(days=dtStartLag_int)
-            + datetime.timedelta(hours=dtStartLag_float * 24),
-            dts_q_calc_all,
-        )
-    )
+    dts_for_calc = dt_all[:q_calc_end_dt_index]
 
-    # 以下ループ
+    # TODO: 以下ループ
+    # 相互相関を求めるために計算値の日時を(スライド全量 / 2)だけずらす
+    dts_for_calc_applied_lag = shiftDts(dts_for_calc, dtStartLag_int, dtStartLag_float)
 
     # 相互相関が最大となるラグを返す
-    largest_lag_sec = calcLag(Q_all, dts_q_calc_all_applied_lag)
+    largest_lag_sec = calcLag(Q_all, dts_for_calc_applied_lag)
     print(f"largest_lag_sec: {largest_lag_sec}")
     dts_applied_largest_lag_sec = list(
         map(
             lambda dt: dt + datetime.timedelta(seconds=int(largest_lag_sec)),
-            dts_q_calc_all,
+            dts_for_calc,
         )
+    )
+
+    dts_for_calc_applied_lag_and_half_slides = shiftDts(
+        dts_applied_largest_lag_sec, dtStartLag_int, dtStartLag_float
     )
 
     lags_sec = []
     for coef in list(
         map(lambda x: x / 10, range(1, 11))
     ):  # [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-        largest_lag_sec = calcLag(Q_all, dts_applied_largest_lag_sec, coef) # TODO: これを更に6時間ずらして相互相関を計算する必要がある
-        print(f"coef: {coef}")
-        print(f"largest_lag_sec: {largest_lag_sec}")
+        largest_lag_sec = calcLag(Q_all, dts_for_calc_applied_lag_and_half_slides, coef)
+        print(f"coef: {coef}, largest_lag_sec: {largest_lag_sec}")
         lags_sec.append(largest_lag_sec)
 
     print(f"min(lags_sec): {min(lags_sec)}")
