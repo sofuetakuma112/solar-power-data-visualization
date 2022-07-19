3fba5bc7dba2e8735668adcba621c58f8ceb862b
sofuetakuma <kawahagi0620@gmail.com>
2022-07-19 14:46:58 +0900

    plot_corr_avg_and_lag.py で描画されるグラフにcorr = 0の補助線を引くよう修正

--- plot_corr_avg_and_lag.py
+++ plot_corr_avg_and_lag.py
@@ -13,10 +13,12 @@ if __name__ == "__main__":
 
     corrs = list(map(lambda row: float(row[3]), rows))
     lags = list(map(lambda row: float(row[4]), rows))
-    
+
+    xmin, xmax = -3, 3
     axes[0][1].scatter(corrs, lags, label="実測値")
     axes[0][1].set_xlabel("相互相関の最大値 / 計算値データ列のリスト長", fontsize=14)
     axes[0][1].set_ylabel("相互相関の最大値に対応するラグ[s]", fontsize=14)
+    axes[0][1].hlines([0], min(corrs), max(corrs), linestyles="dashed")
 
     # print(rows)
 
