1c161aeee8e2cf10524cb4780a3336a803028785
sofuetakuma <kawahagi0620@gmail.com>
2023-06-02 15:52:05 +0900

    自作のシミュレーターで生成した日射量データ同士で周波数解析する処理を実装

---
+++ q_simulation.py
@@ -0,0 +1,124 @@
+import argparse
+from matplotlib import dates
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from utils.frequency_analysis import find_phase_difference, perform_fft
+from utils.init_matplotlib import figsize_px_to_inch, init_rcParams
+import japanize_matplotlib
+from utils.colors import colorlist
+from datetime import datetime
+
+FONT_SIZE = 14
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-dt", type=str)  # グラフ描画したい日付のリスト
+    parser.add_argument("-model", type=str, default="isotropic")  # 'isotropic', 'klucher', 'haydavies', 'reindl', 'king', 'perez'
+    parser.add_argument("-surface_tilt", type=int, default=22)
+    parser.add_argument("-surface_azimuth", type=float, default=185.0)
+    parser.add_argument("-shift_offset", type=int, default=0)
+    parser.add_argument("-max_shift", type=int, default=10)
+    parser.add_argument("-plot_calced_q", action="store_true")
+    return parser.parse_args()
+
+
+def generate_time_array(seconds_in_day, sampling_interval):
+    return np.arange(0, seconds_in_day, sampling_interval)
+
+
+def calculate_output(t, seconds_in_day):
+    output = np.sin((t - 21600) * (2 * np.pi / seconds_in_day))
+    output[output < 0] = 0
+    return output
+
+
+def create_dataframe(t, output):
+    df = pd.DataFrame({"time": pd.to_timedelta(t, unit="s"), "q": output})
+    date = pd.to_datetime("2023-01-01")
+    df["dt"] = pd.to_datetime(df["time"].dt.total_seconds(), unit="s", origin=date)
+    return df
+
+
+def perform_frequency_analysis(q_all, sampling_rate):
+    q_all -= np.mean(q_all)
+    xf_measured, yf_measured = perform_fft(q_all, sampling_rate)
+    return xf_measured, yf_measured
+
+
+def calculate_phase_diffs(shifts, calced_q_all, yf_measured, sampling_rate):
+    phase_diffs = []
+    for shift in shifts:
+        calced_q_all_rolled = np.roll(calced_q_all, shift)
+        calced_q_all_rolled -= np.mean(calced_q_all_rolled)
+        xf_calculated, yf_calculated = perform_fft(calced_q_all_rolled, sampling_rate)
+        phase_diff = find_phase_difference(yf_measured, yf_calculated, len(yf_measured))
+        phase_diffs.append(phase_diff)
+    return np.array(phase_diffs)
+
+
+def plot_phase_graph(shifts, phase_diffs):
+    fig, ax = plt.subplots()
+    ax.plot(shifts, phase_diffs, label="ずれ時間とそれに対応する位相差")
+    ax.set_xlabel("ずれ時間 [s]")
+    ax.set_ylabel("位相差 [rad]")
+    ax.legend()
+
+def plot_calced_q_graph(dt_all, q_all):
+    fig, ax = plt.subplots()
+    ax.plot(
+        dt_all,
+        q_all,
+        label=f"実測値",
+        color=colorlist[0],
+    )
+    ax.set_xlabel("時刻")
+    ax.set_ylabel("日射量 [kW/m$^2$]")
+    ax.xaxis.set_major_formatter(
+        dates.DateFormatter("%H:%M")
+    )
+    ax.legend()
+
+
+def main(args):
+    figsize_inch = figsize_px_to_inch(np.array([1280, 720]))
+    plt.rcParams = init_rcParams(plt.rcParams, FONT_SIZE, figsize_inch)
+    
+    seconds_in_day = 24 * 60 * 60
+    sampling_interval = 1
+    
+    t = generate_time_array(seconds_in_day, sampling_interval)
+    output = calculate_output(t, seconds_in_day)
+    df = create_dataframe(t, output)
+    
+    q_all = df["q"].to_numpy()
+
+    q_all_original = q_all.copy()
+    calced_q_all = q_all.copy()
+
+    dt_all = df["dt"].to_numpy()
+    
+    sampling_rate = 1 / sampling_interval
+    
+    xf_measured, yf_measured = perform_frequency_analysis(q_all, sampling_rate)
+    
+    shifts = np.arange(args.shift_offset, args.max_shift)
+    phase_diffs = calculate_phase_diffs(shifts, calced_q_all, yf_measured, sampling_rate)
+    
+    slope, intercept = np.polyfit(shifts, phase_diffs, 1)
+    print(f"1sずれるごとに増えるラジアン: {slope} [rad]")
+    print(f"phase_diffs[0]: {phase_diffs[0]} [rad]")
+    print(f"phase_diffs[-1]: {phase_diffs[-1]} [rad]")
+    
+    plot_phase_graph(shifts, phase_diffs)
+
+    if args.plot_calced_q:
+        plot_calced_q_graph(dt_all, q_all_original)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
--- shift_phase_graph.py
+++ shift_phase_graph.py
@@ -4,81 +4,11 @@ from matplotlib import pyplot as plt
 import japanize_matplotlib
 from utils.es.load import load_q_and_dt_for_period
 import argparse
+from utils.frequency_analysis import find_phase_difference, perform_fft
 from utils.q import Q
 from utils.correlogram import unify_deltas_between_dts_v2
 import numpy as np
 from scipy.fft import fft, fftfreq
-
-
-def perform_fft(data, sampling_rate):
-    """
-    概要:
-        与えられた時系列データをFFT変換し、その周波数成分の振幅と周波数を返す
-
-    引数:
-        data: list, ndarray - 時系列データ
-        sampling_rate: float - サンプリング周波数
-
-    返り値:
-        xf: ndarray - 周波数軸
-        yf: ndarray - 振幅（複素数）
-    """
-    N = len(data) # 時系列データの長さ
-    T = 1 / sampling_rate # サンプリング周期
-    yf = fft(data)
-    xf = fftfreq(N, T)[: N // 2]
-    return xf, yf
-
-
-def find_fundamental_frequency(xf, yf, N):
-    """
-    概要:
-        フーリエ変換により得られた周波数スペクトルから、最大振幅を示す周波数を見つける関数
-
-    引数:
-        xf(numpy.ndarray): 離散フーリエ変換によって得られた振幅スペクトルの周波数軸上の値が格納された配列
-        yf(numpy.ndarray): 離散フーリエ変換によって得られた振幅スペクトルの振幅が格納された配列
-
-    戻り値:
-        float: 振幅スペクトルの振幅が最大となる周波数
-    """
-    max_index = np.argmax(np.abs(yf[0 : N // 2]))  # 最大振幅を持つ周波数スペクトルデータのインデックスを取得する
-    return xf[max_index]
-
-
-def find_phase_difference(yf_measured, yf_calculated, N):
-    """
-    2つの時系列データから位相差を計算する関数
-    入力された yf_measured と yf_calculated の中で最も強い周波数成分を見つけ、その周波数成分における位相を計算しています。そして、それぞれの位相差を計算して返しています。
-
-    入力:
-        yf_measured: 実測値のデータ
-        yf_calculated: 計算値のデータ
-        N: 時系列データの長さ
-
-    出力:
-        phase_diff: 実測値と計算値の位相差
-    """
-    # np.argmax(yf_measured)で振幅が最大を取る点のインデックスを求める
-    # yf_measured[np.argmax(yf_measured)]で振幅の最大値を求める
-    # np.angleで振幅の最大値（基本周波数）に対応する位相を求める
-    phase_measured = np.angle(yf_measured[np.argmax(np.abs(yf_measured[0 : N // 2]))])
-    phase_calculated = np.angle(
-        yf_calculated[np.argmax(np.abs(yf_calculated[0 : N // 2]))]
-    )
-    print(f"実測データの基本周波数に対応する位相: {phase_measured} [rad]")
-    print(f"計算データの基本周波数に対応する位相: {phase_calculated} [rad]")
-
-    phase_diff = phase_measured - phase_calculated
-    print(f"位相差: {phase_diff} [rad]")
-
-    return phase_diff
-
-
-# 位相から時間へ変換する
-def phase_to_time(phase_diff, fundamental_freq):
-    return phase_diff / (2 * np.pi * fundamental_freq)
-
 
 # > python3 shift_phase_graph.py -dt 2022/04/08 -max_shift 1000
 if __name__ == "__main__":
@@ -126,26 +56,15 @@ if __name__ == "__main__":
     # 実測データをFFTする
     q_all = q_all - np.mean(q_all)
     xf_measured, yf_measured = perform_fft(q_all, sampling_rate)
-    
-    # fundamental_freq_measured = find_fundamental_frequency(
-    #     xf_measured, yf_measured, len(q_all)
-    # )
-    # print(f"実測データの基本周波数: {fundamental_freq_measured} [Hz]")
 
     phase_diffs = np.array([])
     shifts = np.arange(0 + args.shift_offset, args.max_shift, 1)
     for shift in shifts:
-        calced_q_all_rolled = np.roll(calced_q_all, shift) # 計算データをshift分遅らせる
-        
+        calced_q_all_rolled = np.roll(calced_q_all, shift)  # 計算データをshift分遅らせる
+
         # 計算データをFFTする
         calced_q_all_rolled = calced_q_all_rolled - np.mean(calced_q_all_rolled)
         xf_calculated, yf_calculated = perform_fft(calced_q_all_rolled, sampling_rate)
-        
-        # 基本周波数を求める
-        # fundamental_freq_calculated = find_fundamental_frequency(
-        #     xf_calculated, yf_calculated, len(calced_q_all_rolled)
-        # )
-        # print(f"計算データの基本周波数: {fundamental_freq_calculated} [Hz]")
 
         # FFTの結果をプロット
         if args.plot_fft_flag:
@@ -173,7 +92,7 @@ if __name__ == "__main__":
 
     # 横軸ずれ時間、縦軸位相差のグラフの傾きを求める
     slope, intercept = np.polyfit(shifts, phase_diffs, 1)
-    print(f"1sずれるごとに増えるラジアン: {slope} [rad]") # 7.272289386659088e-05 [rad]
+    print(f"1sずれるごとに増えるラジアン: {slope} [rad]")  # 7.272289386659088e-05 [rad]
 
     fig, ax = plt.subplots()
     ax.plot(shifts, phase_diffs, label="ずれ時間とそれに対応する位相差")
