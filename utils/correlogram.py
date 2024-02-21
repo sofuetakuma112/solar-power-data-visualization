83f10933fe0a7d0ae17b0512ab9a24ca1744418d
Your Name <kawahagi0620@gmail.com>
2024-02-21 19:15:25 +0900

    不要なコード、ファイルを削除

--- utils/correlogram.py
+++ utils/correlogram.py
@@ -3,22 +3,6 @@ from utils.date import get_total_seconds
 import numpy as np
 
 
-def test_equality_delta_between_dts(dts, delta=1.0):
-    """
-    日時データのdeltaが1[s]で統一されているかテストする
-    """
-    for i in range(len(dts)):
-        if len(dts) - 1 == i:
-            break
-        diff = dts[i + 1] - dts[i]
-        if diff.total_seconds() != delta:  # 日時のデルタが1sではない
-            print(f"i: {i}")
-            print(f"dts[i + 1]: {dts[i + 1]}")
-            print(f"dts[i]: {dts[i]}")
-            print(diff.total_seconds())
-            raise ValueError("error!")
-
-
 def unify_deltas_between_dts_v2(dts, qs):
     """
     補完した日時データとそれに対応した日射量のリストを取得する
@@ -41,8 +25,5 @@ def unify_deltas_between_dts_v2(dts, qs):
         np.vectorize(get_total_seconds)(dts),
         qs,
     )  # 新しいx列に対応するy列を計算
-
-    # 時系列データの点間が全て1.0[s]かテストする
-    test_equality_delta_between_dts(equally_spaced_dt)
 
     return equally_spaced_dt, completed_q
